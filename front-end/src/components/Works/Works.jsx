13be349e4917f5c0d2c8d3fd9c67835bf8721620
Ashish Auti <ashishauti123@gmail.com>
2025-02-21 19:42:41 +0530

    fix : fix my services, work with brands

--- front-end/src/components/Works/Works.jsx
+++ front-end/src/components/Works/Works.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import "./Works.css";
 import Upwork from "../../img/Upwork.png";
-import Fiverr from "../../img/fiverr.png";
+import freelancer from "../../img/freelancer_logo.png";
 import Amazon from "../../img/amazon.png";
-import Shopify from "../../img/Shopify.png";
-import Facebook from "../../img/Facebook.png";
+import affinity from "../../img/affinity_logo.png";
+import startxlab from "../../img/startxlab_logo.png";
 import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
 import {Link} from 'react-scroll'
@@ -25,14 +25,12 @@ const Works = () => {
           </span>
           <span>Brands & Clients</span>
           <spane>
-            Lorem ispum is simpley dummy text of printing of printing Lorem
+            I have had the opportunity to work with startups, businesses, and 
             <br />
-            ispum is simpley dummy text of printingLorem ispum is simpley dummy
-            text
+            individual clients on freelance, internship, and contract-based 
             <br />
-            y dummy text of printingLorem
-            <br />
-            ispum is simpley dummy text of printing
+            projects.
+          
           </spane>
           <Link to="contact" smooth={true} spy={true}>
             <button className="button s-button">Hire Me</button>
@@ -54,19 +52,21 @@ const Works = () => {
           className="w-mainCircle"
         >
           <div className="w-secCircle">
-            <img src={Upwork} alt="" />
+            <img src={startxlab} alt="" />
           </div>
           <div className="w-secCircle">
-            <img src={Fiverr} alt="" />
+            <img src={affinity} alt="" />
           </div>
           <div className="w-secCircle">
-            <img src={Amazon} alt="" />
+            {/* <img src={Amazon} alt="" /> */}
           </div>{" "}
+          
           <div className="w-secCircle">
-            <img src={Shopify} alt="" />
+            <img src={freelancer} alt="" />
           </div>
+          
           <div className="w-secCircle">
-            <img src={Facebook} alt="" />
+            <img src={Upwork} alt="" />
           </div>
         </motion.div>
         {/* background Circles */}
